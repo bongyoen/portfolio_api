0037fc586061cc09deedd75248a3323aa4a237be
김봉연 <kuhj7593@gmail.com>
2023-10-10 21:25:41 +0900

    init

---
+++ src/main/java/com/co/portfolio_api/PortfolioApiApplication.java
@@ -0,0 +1,13 @@
+package com.co.portfolio_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioApiApplication.class, args);
+    }
+
+}
