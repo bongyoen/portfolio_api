dc7eb3078bc9888178ce82d282d4ad5313437478
김봉연 <kuhj7593@gmail.com>
2023-11-28 16:58:56 +0900

    api 테스트!

--- src/main/java/com/co/portfolio_api/business/menu/MenuController.java
+++ src/main/java/com/co/portfolio_api/business/menu/MenuController.java
@@ -6,18 +6,24 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
 @RequiredArgsConstructor
-@RequestMapping("/menu")
 public class MenuController {
 
     private final MenuService service;
-    @GetMapping(value = "/webHeaders")
+    @GetMapping(value = "/menu/webHeaders")
     public ResponseEntity<MenuDto.WebHeaderRslt> getWebHeaders() {
+        try {
+            return new ResponseEntity<>(service.getWebHeaders(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+    }
+    @GetMapping(value = "/webHeaders")
+    public ResponseEntity<MenuDto.WebHeaderRslt> webHeaders() {
         try {
             return new ResponseEntity<>(service.getWebHeaders(), HttpStatus.OK);
         } catch (Exception e) {
